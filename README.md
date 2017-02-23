6288455b1850da2ea24378d8f08bfd2c59d3d84e
Z <no_mail>
2017-02-23 20:49:44 +0300

    added streaming and ctrl+c handling

--- README.md
+++ README.md
@@ -4,3 +4,20 @@ Userland CLI utility to capture frames from Vimicro vc088x SoC camera (Viewsonic
 Right now this is a quick and dirty example of squeezing image data from a very closed and proprietary VC088x chip. The official driver for it is not v4l-compatible and there is no source code for a libcamera library using it.
 
 So if you're about to dig into this mess (or a similar mess from another brand), this code will help you. A little.
+
+Usage EXAMPLES:
+
+Building:
+```
+    ./compile.sh
+```
+
+Capturing raw stream:
+```
+    ./ioctl | pv > raw.yuv
+```
+
+Encoding pipeline (experimental):
+```
+    ./ioctl | ffmpeg -pix_fmt yuyv422 -s 1280x1024 -i - -vcodec libx264 -preset fast capture.mp4
+```
--- ioctl.c
+++ ioctl.c
@@ -5,8 +5,10 @@
 #include <stdlib.h>     // exit
 #include <sys/mman.h>   // mmap, munmap
 #include <memory.h>     // memset
+#include <signal.h>
 
 #define UINT32 unsigned int
+#define LOG(...) fprintf(stderr, __VA_ARGS__)
 
 #include "vc088x_cif_drv.h"
 #include "android_pmem.h"
@@ -28,23 +30,23 @@ void ioctl_getinfo(int file_desc)
 
   ret_val = ioctl(file_desc, V8CIF_SNR_GETINFO, &sinfo);
   if (ret_val < 0) {
-    printf ("ioctl req getinfo failed:%d\n", ret_val);
+    LOG("ioctl req getinfo failed:%d\n", ret_val);
     exit(-1);
   }
 
-  printf("Command succeeded, info:\n");
+  LOG("Command succeeded, info:\n");
 
-  printf(" totalItems: %d\n", sinfo.totalItems);
-  printf(" type: %d", sinfo.type);
-  printf(" jpegSupported: %d\n", sinfo.jpegSupported);
-  printf("\n Modes:\n");
+  LOG(" totalItems: %d\n", sinfo.totalItems);
+  LOG(" type: %d", sinfo.type);
+  LOG(" jpegSupported: %d\n", sinfo.jpegSupported);
+  LOG("\n Modes:\n");
 
   int i;
   if (sinfo.totalItems > SNR_ITEM_NUM) {
     sinfo.totalItems = SNR_ITEM_NUM;
   }
   for (i = 0; i < sinfo.totalItems; ++i) {
-     printf("  %d x %d\n", sinfo.width[i], sinfo.height[i]);
+     LOG("  %d x %d\n", sinfo.width[i], sinfo.height[i]);
   }
 }
 
@@ -59,23 +61,25 @@ void ioctl_power(int file_desc, int on)
   }
 
   if (ret_val < 0) {
-    printf ("ioctl req power failed:%d\n", ret_val);
+    LOG("ioctl req power failed:%d\n", ret_val);
     exit(-1);
   }
 }
+
+int g_runLoop = 1;  // When this goes 0, program should stop capture loop
 
 void ioctl_grab(int file_desc, unsigned char *framebuf, UINT32 hwoffset)
 {
     int ret_val;
 
-    printf("Setting resolution...\n");
+    LOG("Setting resolution...\n");
     ret_val = ioctl(file_desc, V8CIF_SNR_CFG, 1);  // Set max resolution
     if (ret_val < 0) {
-        printf ("ioctl set resolution failed:%d\n", ret_val);
+        LOG("ioctl set resolution failed:%d\n", ret_val);
         exit(-1);
     }
 
-    printf("Configuring grab...\n");
+    LOG("Configuring grab...\n");
     V8CIFPATHINFO cpath;
     cpath.dst_type = 0;
     cpath.dst_width = FRAME_WIDTH;
@@ -88,45 +92,36 @@ void ioctl_grab(int file_desc, unsigned char *framebuf, UINT32 hwoffset)
 
     ret_val = ioctl(file_desc, V8CIF_PREPATH_CFG, &cpath);
     if (ret_val < 0) {
-        printf ("ioctl grab failed:%d\n", ret_val);
+        LOG("ioctl grab failed:%d\n", ret_val);
         exit(-1);
     }
 
     /*ret_val = ioctl(file_desc, V8CIF_SETPREVIEWSTOP, 0);
     if (ret_val < 0) {
-        printf ("ioctl previewstop failed:%d\n", ret_val);
+        LOG("ioctl previewstop failed:%d\n", ret_val);
         exit(-1);
     }*/
 
-    printf("Begin cycle...\n");
-    int gotdata = 0;
-    while (!gotdata) {
+    LOG("Begin cycle...\n");
+    while (g_runLoop) {
         V8CIFFRMINFO frinfo;
         frinfo.dst_addr_y = (UINT32)hwoffset;
         frinfo.dst_addr_uv = (UINT32)hwoffset + FRAME_WIDTH * FRAME_HEIGHT;
         frinfo.cap_mode = CAP_NORMAL; // Manufacturer uses 0
 
-        printf(".");
         ret_val = ioctl(file_desc, V8CIF_PREPATH_GETFRM, &frinfo);
-        printf("[D] ioctl packet V8CIF_PREPATH_GETFRM our: %08X, manuf: %08X\n", V8CIF_PREPATH_GETFRM, 0x800C6308);
+        //LOG("[D] ioctl packet V8CIF_PREPATH_GETFRM our: %08X, manuf: %08X\n", V8CIF_PREPATH_GETFRM, 0x800C6308);
         if (ret_val < 0) {
-            printf ("ioctl grab failed:%d\n", ret_val);
+            LOG("ioctl grab failed:%d\n", ret_val);
             exit(-1);
         }
-
-        printf("[D] dst_addr: %08X\n", frinfo.dst_addr_y);
-        if (framebuf[0] || framebuf[100] || framebuf[200]) {  // Randomly test some pixels for data
-            printf("We have the juice!\n");
-            gotdata = 1;
-        } else {
-            sleep(1);
-        }
+        
+        // TODO: decode?
+        // Stream to stdout
+        fwrite(framebuf, 1, FRAME_SIZE_BYTES, stdout);
+        usleep(200000);
     }
-
-    printf("Got the frame! Saving...\n");
-    FILE *fp = fopen("./capture.raw", "wb");
-    fwrite(framebuf, 1, FRAME_SIZE_BYTES, fp);
-    fclose(fp);
+    LOG("Finished cycle.\n");
 }
 
 int opendev(const char *devname, int mode)
@@ -135,7 +130,7 @@ int opendev(const char *devname, int mode)
     file_desc = open(devname, 2, mode);  // [XXX] Manufacturer's code uses flag 2, but why?!
 
     if (file_desc < 0) {
-        printf("Can't open device file: %s\n", devname);
+        LOG("Can't open device file: %s\n", devname);
         exit(-1);
     }
     
@@ -151,13 +146,13 @@ int opendev(const char *devname, int mode)
 void *PhyMemAlloc(int pmem_dev, struct pmem_region *sub)
 {
     const size_t pagesize = getpagesize();
-    printf("[D] Page size: %d\n", pagesize);
+    LOG("[D] Page size: %d\n", pagesize);
     
     int size = sub->len;
     size = (size + pagesize-1) & ~(pagesize-1);
     
     void *ptr = mmap(0, size, PROT_READ | PROT_WRITE, MAP_SHARED, pmem_dev, 0);  // prot == 3, flags == 1
-    printf("[D] ioctl command packet (should be 0x40047001): %08X\n", PMEM_MAP);
+    LOG("[D] ioctl command packet (should be 0x40047001): %08X\n", PMEM_MAP);
 
     // Should be PMEM_MAP, but manufacturer uses "read" version: 0x40047001, and a weird argument
     //int err = ioctl(pmem_dev, PMEM_MAP, sub);
@@ -165,17 +160,17 @@ void *PhyMemAlloc(int pmem_dev, struct pmem_region *sub)
     int err = ioctl(pmem_dev, 0x40047001, &hwptr);
     sub->offset = hwptr;
     if (err < 0) {
-        printf("PMEM_MAP failed (%s), mFD=%d, sub.offset=%lu, sub.size=%lu",
+        LOG("PMEM_MAP failed (%s), mFD=%d, sub.offset=%lu, sub.size=%lu",
                 err, pmem_dev, sub->offset, sub->len);
         exit(-1);
     } else {
-        printf("PMEM_MAP success, sub.offset=%lu, sub.size=%lu\n",
+        LOG("PMEM_MAP success, sub.offset=%lu, sub.size=%lu\n",
                 sub->offset, sub->len);
     }
     
-    printf("Allocated userland ptr: %08X, hw offset: %08X\n", ptr, hwptr);
+    LOG("Allocated userland ptr: %08X, hw offset: %08X\n", ptr, hwptr);
     if (ptr) {
-        printf("Wiping %d bytes of memory...\n", sub->len);
+        LOG("Wiping %d bytes of memory...\n", sub->len);
         memset(ptr, 0, size);
     }
     
@@ -185,13 +180,13 @@ void *PhyMemAlloc(int pmem_dev, struct pmem_region *sub)
 void PhyMemFree(int pmem_dev, void *ptr, struct pmem_region *sub)
 {
     if (munmap(ptr, sub->len) > 0) {
-        printf("Cannot munmap ptr: %08X, size: %d\n", ptr, sub->len);
+        LOG("Cannot munmap ptr: %08X, size: %d\n", ptr, sub->len);
     }
     
     // Manufacturer doesn't use this part
     /*int err = ioctl(pmem_dev, PMEM_UNMAP, &sub);
     if (err < 0) {
-        printf("PMEM_UNMAP failed (%d), mFD=%d, sub.offset=%lu, sub.size=%lu\n",
+        LOG("PMEM_UNMAP failed (%d), mFD=%d, sub.offset=%lu, sub.size=%lu\n",
                 err, pmem_dev, sub->offset, sub->len);
         exit(-1);
     }*/
@@ -202,20 +197,28 @@ void PhyMemInfo(int pmem_dev)
     int ret_val, rsize;
     ret_val = ioctl(pmem_dev, PMEM_GET_TOTAL_SIZE, &rsize);
     if (ret_val < 0) {
-        printf ("ioctl PMEM_GET_TOTAL_SIZE failed: %d\n", ret_val);
+        LOG("ioctl PMEM_GET_TOTAL_SIZE failed: %d\n", ret_val);
         exit(-1);
     }
-    printf("Total physical memory size: %d, retval: %d\n", rsize, ret_val);
+    LOG("Total physical memory size: %d, retval: %d\n", rsize, ret_val);
+}
+
+void sig_handler(int signum)
+{
+    g_runLoop = 0;
+    LOG("Stopping...\n");
 }
 
 void main()
 {
+    signal(SIGINT, sig_handler);
+
     int camera_dev, pmem_dev, ret_val;
     camera_dev = opendev(DEVICE_FILE_NAME, O_RDONLY);
     pmem_dev = opendev(PMEM_VDEC_FILE_NAME, O_RDONLY);
 
     // Poke physmem mapper
-    printf("Mapping memory...\n");
+    LOG("Mapping memory...\n");
     struct pmem_region sub = { 0, FRAME_SIZE_BYTES };
     void *ptr = PhyMemAlloc(pmem_dev, &sub);
 
